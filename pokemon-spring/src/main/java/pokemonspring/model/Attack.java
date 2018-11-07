f2f716fa8475489b61dfff9fd7f0c27deca78de5
LunarShadow <jseumo@g.clemson.edu>
2018-11-06 20:30:24 -0500

    Renamed files correctly

--- pokemon-spring/src/main/java/pokemonspring/model/Attack.java
+++ pokemon-spring/src/main/java/pokemonspring/model/Attack.java
@@ -1,28 +1,7 @@
 package pokemonspring.model;
 
-import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
-@Table(name ="type")
-public class Type implements Serializable{
-    @Id
-    @Column(name="type_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name="type_name")
-    private String type;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
+public class Attack implements Serializable{
+ 
 }
