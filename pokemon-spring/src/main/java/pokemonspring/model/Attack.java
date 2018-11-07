7aa1439355f08515b86dcf75d2e9adc1478ec56d
Calvin Doan <spartanb174@gmail.com>
2018-11-06 22:04:47 -0500

    Added Attack DTO
    Attack DTO

--- pokemon-spring/src/main/java/pokemonspring/model/Attack.java
+++ pokemon-spring/src/main/java/pokemonspring/model/Attack.java
@@ -1,7 +1,31 @@
 package pokemonspring.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
-public class Attack implements Serializable{
+@Entity
+@Table(name = "attack")
+public class Attack implements Serializable {
+
+    @Id
+    @Column(name="attack_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="attack_name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
  
 }
