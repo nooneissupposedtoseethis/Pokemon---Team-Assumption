bac2763b1f591f30de7177cfdc8c6dde6978835e
LunarShadow <jseumo@g.clemson.edu>
2018-11-06 20:23:34 -0500

    Type update

--- pokemon-spring/src/main/java/pokemonspring/model/Attack.java
+++ pokemon-spring/src/main/java/pokemonspring/model/Attack.java
@@ -1,7 +1,28 @@
 package pokemonspring.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
-public class Attack implements Serializable {
+@Entity
+@Table(name ="type")
+public class Type implements Serializable{
+    @Id
+    @Column(name="type_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name="type_name")
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
