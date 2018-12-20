8ab21e5a8379d38af12339dc4cfbd3d13a0a75ef
Cyntia Seumo <cyntia.seumo@ruralsourcing.com>
2018-12-20 17:13:23 -0500

    template of individual pokemone

--- pokemon-react/src/index.js
+++ pokemon-react/src/index.js
@@ -1,80 +1,49 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import './index.css'
 
 
-const URL ='http://localhost:8080//pokedex/list';
+const URL ='http://localhost:8080//pokedex//pokemon/3';
 
-class Pokemon extends React.Component {
-   state = {
-       isLoading: true,
-       pokeData: [],
-       error: null
-   }
+class Pokemon extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { 
+            pokeData: [],
+            types: [],
+            attack1: [],
+            attack2:[],
+            error: null,
+            isLoading: true
+        }
+    }
 
-   render(){
-       const { isLoading, pokeData, error} = this.state;
-       return ( 
-            <React.Fragment>
-                {error ? <p>{error.message}</p> : null}
-
-                {!isLoading ? (
-                    
-                    pokeData.map(pokeData => {
-                        const { pokemonName,pokemonPicture, pokemonType, attackName1, attackName2} = pokeData;
-                        return(
-                            <div key={pokemonName}>
-                                <h1>{pokemonName}</h1>
-                                <img src={pokemonPicture} width="100" alt="here lies your pokemon" />
-                                <p>Type: {pokemonType}</p>
-                                <p>Attacks: {attackName1}, {attackName2}</p>
-                            </div>
-                        );
-                    })
-                ) : (
-                    <h3>Loading...</h3>
-                )}
-            </React.Fragment>
-       );
-   }
-
-   componentDidMount(){
-       this.getData();
-   }
-
-  /*fetchData(){
+    componentDidMount(){
        fetch(URL)
        .then(response => response.json())
-       .then(data => 
+       .then(data =>(
             this.setState({
                 pokeData: data,
+                types:data.types[0],
+                attack1: data.attacks[0],
+                attack2: data.attacks[1],
                 isLoading: false,
-            })
-        )
-        .catch(error => this.setState({error, isLoading: false}));
-  } */
-
-   getData(){
-       axios.get(URL)
-       .then(response => 
-            response.data.map(pokeData => ({
-                pokemonName: `${pokeData.pokemonName}`,
-                pokemonPicture: `${pokeData.pokemonPicture}`,
-                pokemonType: `${pokeData.types[0].typeName}`,
-                attackName1: `${pokeData.attacks[0].attackName}`,
-                attackName2: `${pokeData.attacks[1].attackName}`
-
             }))
         )
-        .then(pokeData => {
-            this.setState({
-                pokeData,
-                isLoading: false
-            });
-        })
-       .catch(error => this.setState({error, isLoading: false}));
-   }
+        .catch(error => this.setState({error, isLoading: false}));
+    }
+
+    render(){
+      return(
+        <div key={this.state.pokeData.pokemonId}>
+            <h1>{this.state.pokeData.pokemonName}</h1>
+            <img src={this.state.pokeData.pokemonPicture} width="100" alt="here lies your pokemon" />
+            <p>Type: {this.state.types.typeName}</p>
+            <p>Attacks: {this.state.attack1.attackName}, {this.state.attack2.attackName}</p>
+        </div>
+      );
+    }
 }
+
 
 ReactDOM.render(<Pokemon />, document.getElementById('pokemonName'));
